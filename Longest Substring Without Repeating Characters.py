75232c02cd936fbe077cef60e2850c23cd9317c1
waithope <yunqiang.gan@gmail.com>
2018-06-17 14:10:26 +0800

    longest substring with unittest

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -22,4 +22,13 @@ def lengthOfLongestSubstring(s):
     return maxlen
 
 
-print(lengthOfLongestSubstring('bacabcbb'))
+import unittest
+
+class TestLongestString(unittest.TestCase):
+  def test_longest_string(self):
+    self.assertEqual(lengthOfLongestSubstring('abcabcbb'), 3)
+    self.assertEqual(lengthOfLongestSubstring('bbbbb'), 1)
+    self.assertEqual(lengthOfLongestSubstring('pwwkew'), 3)
+
+if __name__ == '__main__':
+  unittest.main()
