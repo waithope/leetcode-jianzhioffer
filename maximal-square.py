920f5a0c49cad61d873b428831e97b3ec67ac518
waithope <yunqiang.gan@gmail.com>
2018-07-06 16:28:11 +0800

    maximal square with o(n) space

--- maximal-square.py
+++ maximal-square.py
@@ -30,13 +30,36 @@ def maximalSquare(matrix):
       maxArea = max(maxArea, dp[i][j])
   return maxArea**2
 
+## Time Complexity O(mn)
+## Space Complexity O(n)
+def maximalSquare_v2(matrix):
+  if not matrix or not matrix[0]:
+    return 0
+
+  m = len(matrix)
+  n = len(matrix[0])
+  dp = [1 if int(x) == 1 else 0 for x in matrix[0]]
+  prev = 0
+  maxArea = 0 if sum(dp) == 0 else 1
+  for i in range(1, m):
+    for j in range(n):
+      if j == 0:
+        prev = dp[0]
+        dp[j] = 1 if int(matrix[i][j]) == 1 else 0
+      else:
+        temp = dp[j]    # save previous row's state in case overriding
+        dp[j] = min(prev, dp[j-1], dp[j]) + 1 if int(matrix[i][j]) == 1 else 0
+        prev = temp
+      maxArea = max(maxArea, dp[j])
+  return maxArea**2
+
 
 import unittest
 
 class TestMaximalSquare(unittest.TestCase):
   def test_maximal_square(self):
-    self.assertEqual(maximalSquare([['1','0','1','0','0'],['1','0','1','1','1'],['1','1','1','1','1'],['1','0','0','1','0']]), 4)
-    self.assertEqual(maximalSquare([['0','1','1','1','0'],['1','1','1','1','0'],['0','1','1','1','1'],['0','1','1','1','1'],['0','0','1','1','1']]), 9)
+    self.assertEqual(maximalSquare_v2([['1','0','1','0','0'],['1','0','1','1','1'],['1','1','1','1','1'],['1','0','0','1','0']]), 4)
+    self.assertEqual(maximalSquare_v2([['0','1','1','1','0'],['1','1','1','1','0'],['0','1','1','1','1'],['0','1','1','1','1'],['0','0','1','1','1']]), 9)
 
 
 if __name__ == '__main__':
