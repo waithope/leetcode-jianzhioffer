e6c7d4b86d71bebfb04e8489bee06c10551a528b
waithope <yunqiang.gan@gmail.com>
2018-07-18 00:13:46 +0800

    majority element O(n) solution

--- array/majority-elementII.py
+++ array/majority-elementII.py
@@ -7,7 +7,7 @@
 ## Input: [1,1,1,3,3,2,2,2]
 ## Output: [1,2]
 
-## Time O(n) Space O(1)
+## Time O(nlog(n)) Space O(1)
 def majorityElement(nums):
   """
   :type nums: List[int]
@@ -32,13 +32,37 @@ def majorityElement(nums):
   return res
 
 
+## Boyer–Moore majority vote algorithm
+## Time O(n) Space O(1)
+def majorityElement_v2(nums):
+  if nums == []:
+    return []
+
+  candidate1, candidate2, cnt1, cnt2 = None, None, 0, 0
+  for num in nums:
+    if num == candidate1:
+      cnt1 += 1
+    elif num == candidate2:
+      cnt2 += 1
+    elif cnt1 == 0:
+      cnt1 = 1
+      candidate1 = num
+    elif cnt2 == 0:
+      cnt2 = 1
+      candidate2 = num
+    else:
+      cnt1 -= 1
+      cnt2 -= 1
+  return [x for x in (candidate1, candidate2) if nums.count(x) > len(nums)//3]
+
 import unittest
 
 class TestMajorityElementsII(unittest.TestCase):
   def test_majority_elementsII(self):
-    self.assertEqual(majorityElement([1,2]), [1,2])
-    self.assertEqual(majorityElement([3,2,3]), [3])
-    self.assertEqual(majorityElement([1,1,1,3,3,2,2,2]), [1,2])
+    self.assertEqual(majorityElement_v2([0,0,0]), [0])
+    self.assertEqual(majorityElement_v2([1,2]), [1,2])
+    self.assertEqual(majorityElement_v2([3,2,3]), [3])
+    self.assertEqual(majorityElement_v2([1,1,1,3,3,2,2,2]), [1,2])
 
 
 if __name__ == '__main__':
