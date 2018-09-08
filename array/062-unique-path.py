b92846d7b64ffbb7eeaff4d5e6dda85ff38d9b0d
waithope <yunqiang.gan@gmail.com>
2018-09-08 11:38:31 +0800

    add o(n) space method

--- array/062-unique-path.py
+++ array/062-unique-path.py
@@ -26,20 +26,19 @@ def uniquePaths(m, n):
   :rtype: int
   """
   ## time complexity O(n**2) space complexity O(m*n)
-  path = [[1]*n for i in range(m)]
+  # path = [[1]*n for i in range(m)]
 
-  for i in range(1, m):
-     for j in range(1, n):
-         path[i][j] = path[i-1][j] + path[i][j-1]
-  return path[m-1][n-1]
+  # for i in range(1, m):
+  #    for j in range(1, n):
+  #        path[i][j] = path[i-1][j] + path[i][j-1]
+  # return path[m-1][n-1]
 
   ## time complexity O(n**2) space complexity O(n)
-  ## this method exists some errors, albeit passed tests
-  # dp = [1]*n
-  # for i in range(1, m):
-  #     for j in range(1, n):
-  #         dp[j] = dp[j-1] + dp[j]
-  # return dp[-1]
+  dp = [1]*n
+  for i in range(1, m):
+      for j in range(1, n):
+          dp[j] = dp[j-1] + dp[j]
+  return dp[-1]
 
 
 import unittest
