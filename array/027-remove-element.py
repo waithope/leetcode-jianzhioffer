c3796b32649ff679ad97c55440886ccf4b69b00d
waithope <yunqiang.gan@gmail.com>
2018-09-27 17:34:43 +0800

    remove element

--- array/027-remove-element.py
+++ array/027-remove-element.py
@@ -1,15 +1,21 @@
-## Description
-## Given an array nums and a value val, remove all instances of
-## that value in-place and return the new length.
-## Do not allocate extra space for another array, you must do this
-## by modifying the input array in-place with O(1) extra memory.
-## The order of elements can be changed. It doesn't matter
-## what you leave beyond the new length.
+'''
+           Remove Element
+===================================
+Given an array nums and a value val, remove all instances of
+that value in-place and return the new length.
+Do not allocate extra space for another array, you must do this
+by modifying the input array in-place with O(1) extra memory.
+The order of elements can be changed.
+It doesn't matter what you leave beyond the new length.
 
-## Given nums = [3,2,2,3], val = 3,
-## Your function should return length = 2
-## Given nums = [0,1,2,2,3,0,4,2], val = 2,
-## Your function should return length = 5
+Example 1:
+Given nums = [3,2,2,3], val = 3,
+Your function should return length = 2
+
+Example 2:
+Given nums = [0,1,2,2,3,0,4,2], val = 2,
+Your function should return length = 5
+'''
 
 def removeElement(nums, val):
   """
@@ -38,4 +44,3 @@ class TestRemoveElement(unittest.TestCase):
 
 if __name__ == '__main__':
   unittest.main()
-
