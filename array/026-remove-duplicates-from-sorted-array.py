0f62e7b595fbffccbcc49ced4daf1299e6d69a26
waithope <yunqiang.gan@gmail.com>
2018-09-27 12:10:12 +0800

    remove duplicates from sorted array

--- array/026-remove-duplicates-from-sorted-array.py
+++ array/026-remove-duplicates-from-sorted-array.py
@@ -1,14 +1,23 @@
-## Description
-## Given a sorted array nums, remove the duplicates in-place
-## such that each element appear only once and return the new length.
-## Do not allocate extra space for another array, you must do this by
-## modifying the input array in-place with O(1) extra memory.
+'''
+  Remove Duplicates from Sorted Array
+=========================================
+Given a sorted array nums, remove the duplicates in-place
+such that each element appear only once and return the new length.
+Do not allocate extra space for another array,
+you must do this by modifying the input array in-place with O(1) extra memory
 
-## Given nums = [1,1,2],
-## Your function should return length = 2, with the first two elements
-## of nums being 1 and 2 respectively.
-## It doesn't matter what you leave beyond the returned length.
+Example 1:
+Given nums = [1,1,2],
+Your function should return length = 2, with the first two elements
+of nums being 1 and 2 respectively.
+It doesn't matter what you leave beyond the returned length.
 
+Example 2:
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+Your function should return length = 5, with the first five elements of nums
+being modified to 0, 1, 2, 3, and 4 respectively.
+It doesn't matter what values are set beyond the returned length.
+'''
 
 def removeDuplicates(nums):
   """
@@ -25,7 +34,6 @@ def removeDuplicates(nums):
       tail += 1
       nums[tail] = nums[i]
   return tail+1
-
 
 import unittest
 
