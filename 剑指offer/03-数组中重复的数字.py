57b75eff037b7521ee804cf1f51219af728bbb91
waithope <yunqiang.gan@gmail.com>
2019-02-25 21:37:20 +0800

    add jianzhi offer

---
+++ 剑指offer/03-数组中重复的数字.py
@@ -0,0 +1,82 @@
+# -*- coding:utf-8 -*-
+'''
+    数组中重复的数字
+======================
+找出数组中重复的数字。
+在一个长度为n的数组里的所有数字都在0~n-1的范围内。数组中某些数字是重复的，
+但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个
+重复的数字。例如，如果输入长度为7的数组{2, 3, 1, 0, 2, 5, 3}，那么对应
+的输出是重复的数字2或3。
+'''
+
+def duplicate1(numbers):
+    '''
+    修改原数组列表，时间O(n), 空间O(1)
+    '''
+    if numbers is None or len(numbers) <= 0:
+        return False, None
+    for num in numbers:
+        if num < 0 or num >= len(numbers):
+            return False, None
+    for i in range(len(numbers)):
+        while numbers[i] != i:
+            if numbers[i] == numbers[numbers[i]]:
+                return True, numbers[i]
+            else:
+                index = numbers[i]
+                numbers[i], numbers[index] = numbers[index], numbers[i]
+    return False, None
+
+'''
+    数组中重复的数字
+======================
+找出数组中重复的数字。
+在一个长度为n+1的数组里的所有数字都在1~n的范围内。所以数组中至少有一个数字
+是重复的。请找出数组中任意一个重复的数字。例如，如果输入长度为8的数组
+{2, 3, 5, 4, 3, 2, 6, 7}，那么对应的输出是重复的数字2或3。
+'''
+
+def duplicate2(numbers):
+    '''
+    不修改原数组，时间O(nlgn)，空间O(1)
+    '''
+    def count_range(numbers, low, high):
+        count = 0
+        for num in numbers:
+            if num >= low and num <= high:
+                count += 1
+        return count
+
+    if len(numbers) <= 0:
+        return False, None
+    for num in numbers:
+        if num < 1 and num > len(numbers) - 1:
+            return False, None
+
+    start, end = 1, len(numbers) - 1
+    while start <= end:
+        mid = start + (end - start) // 2
+        count = count_range(numbers, start, mid)
+        if start == end:
+            if count > 1:
+                return True, start
+        if count <= mid - start + 1:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return False, None
+
+
+
+
+import unittest
+
+class TestDuplicate(unittest.TestCase):
+    def test_duplicate(self):
+        self.assertEqual(duplicate1([2,3,1,0,2,5,3]), (True, 2))
+        self.assertEqual(duplicate2([2,3,5,4,3,2,6,7]), (True, 3))
+
+if __name__ == '__main__':
+    unittest.main()
+
+
---
+++ 剑指offer/04-二维数组中的查找.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+'''
+    二维数组中的查找
+======================
+在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下
+递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组
+中是否含有整数。如果找到该整数，则返回True；否则，返回False。
+'''
+
+def find(matrix, number):
+    '''
+    时间O(n)，空间O(1)
+    '''
+    if len(matrix) <= 0:
+        return False
+    rows = len(matrix)
+    cols = len(matrix[0])
+    i, j = 0, cols - 1
+    while i < rows and j > 0:
+        if matrix[i][j] == number:
+            return True
+        elif matrix[i][j] > number:
+            j -= 1
+        else:
+            i += 1
+    return False
+
+
+import unittest
+
+class TestFind(unittest.TestCase):
+    def test_find(self):
+        self.assertEqual(find([[1, 2, 8, 9],
+                               [2, 4, 9, 12],
+                               [4, 7, 10, 13],
+                               [6, 8, 11, 15]], 3), False)
+
+if __name__ == '__main__':
+    unittest.main()
