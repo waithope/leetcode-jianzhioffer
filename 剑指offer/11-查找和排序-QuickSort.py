7fa59c5d8f5543d507b764861242c7533247793b
waithope <yunqiang.gan@gmail.com>
2019-03-25 11:30:25 +0800

    quick sort

--- 剑指offer/11-查找和排序-QuickSort.py
+++ 剑指offer/11-查找和排序-QuickSort.py
@@ -32,9 +32,22 @@ def quickSort(nums, start, end):
 
 
 if __name__ == '__main__':
-    data = [0,-1,-8,10,11,5,6]
-    quickSort(data, 0, 6)
-    print(data)
+    # test1
+    data1 = [0,-1,-8,10,11,5,6]
+    quickSort(data1, 0, len(data1)-1)
+    assert(data1 == [-8,-1,0,5,6,10,11])
+    # test2
+    data2 = [5,4,3,2,1,0,-1,-2]
+    quickSort(data2, 0, len(data2)-1)
+    assert(data2 == [-2,-1,0,1,2,3,4,5])
+    # test3
+    data3 = [1,1,1,2,2,2,0,0,0,-1,-1,-1]
+    quickSort(data3, 0, len(data3)-1)
+    assert(data3 == [-1,-1,-1,0,0,0,1,1,1,2,2,2])
+    # test4
+    data4 = [13,4,12,1,14,11,7,3,9,10,5,2,6,8]
+    quickSort(data4, 0, len(data4)-1)
+    assert(data4 == [1,2,3,4,5,6,7,8,9,10,11,12,13,14])
 
 
 
