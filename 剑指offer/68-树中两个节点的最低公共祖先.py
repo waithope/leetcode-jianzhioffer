45191b01febe075194824feb96b01c4aedcdae68
waithope <yunqiang.gan@gmail.com>
2019-06-13 16:54:22 +0800

    树中两个节点的最低公共祖先

--- 剑指offer/68-树中两个节点的最低公共祖先.py
+++ 剑指offer/68-树中两个节点的最低公共祖先.py
@@ -152,6 +152,90 @@ def lowestCommonAncestor_Tarjan(root, node1, node2):
         return None
     return tarjan(root, node1, node2)
 
+import math
+def lowestCommonAncestor_RMQ(root, node1, node2):
+    '''
+    思路：最近公共祖先问题可以转化为RMQ区间最值查询问题，RMQ是一种先进行
+    O(nlogn)预处理，然后O(1)在线查询的算法。所谓区间最值查询，是指在一个
+    数组中查找两个指定索引中最小值的位置。先通过DFS获取树的欧拉序列以及对应
+    的深度序列(每个节点在树中的深度)，然后构建Sparse Table，这个表就是用于
+    解决给定区间求最值的问题，一旦构建完成后，查询的时间为O(1)。
+    RMQ：https://blog.csdn.net/weixin_42001089/article/details/83590686
+    欧拉序列：https://www.cnblogs.com/pealicx/p/6859901.html
+    树的欧拉序列是对树进行深度优先遍历DFS的一种序列。有两种形式：1、在
+    每个节点进和出都加进序列，最终每个节点恰好出现两次。2、在遍历的时候只要到达一个
+    节点就加进序列。
+    '''
+    def depthFirstSearch(root, d):
+        depths.append(d)
+        eulers.append(root)
+        if root not in first.keys():
+            first[root] = len(eulers)
+        if root.pLeft is not None:
+            depthFirstSearch(root.pLeft, d + 1)
+            eulers.append(root)
+            depths.append(d)
+        if root.pRight is not None:
+            depthFirstSearch(root.pRight, d + 1)
+            eulers.append(root)
+            depths.append(d)
+
+    def buildSparseTable(length):
+        k = int(math.log(length, 2))
+        for i in range(length):
+            st[i + 1] = [i + 1]
+        for j in range(1, k + 1):
+            i = 1
+            pos = i + 2 ** j - 1
+            while pos <= length:
+                a = st[i][j - 1]
+                b = st[i + 2 ** (j - 1)][j - 1]
+                if depths[a - 1] <= depths[b - 1]:
+                    st[i].append(a)
+                else:
+                    st[i].append(b)
+                i += 1
+                pos += 1
+
+    def RMQ(m, n):
+        k = int(math.log(n - m + 1, 2))
+        a = st[m][k]
+        b = st[n - 2 ** k + 1][k]
+        if depths[a - 1] <= depths[b - 1]:
+            return a
+        else:
+            return b
+
+    def LCA(node1, node2):
+        if first[node1] < first[node2]:
+            res = RMQ(first[node1], first[node2])
+        else:
+            res = RMQ(first[node2], first[node1])
+        return eulers[res - 1]
+
+
+    if (not isinstance(root, BinaryTreeNode)
+        or not isinstance(node1, BinaryTreeNode)
+        or not isinstance(node2, BinaryTreeNode)):
+        return None
+
+    # eulers代表欧拉序列， depths记录序列每个节点的深度
+    eulers, depths = [], []
+
+    # first记录每一个元素在欧拉序列中首次出现的位置
+    first = {}
+
+    # st记录每段区间最值，它的结构是这样{1: [1, 1, 1], 2: [2, 2, 11], ...}
+    # 比如2: [2, 2, 11]代表的意义就是从第二个位置开始，长度为1的区间中(本身)
+    # 深度最浅元素的位置是2，长度为2的区间中深度最浅元素的位置是2；长度为4的区间中
+    # (本身)深度最浅元素的位置是11
+    st = {}
+    depthFirstSearch(root, 1)
+    if node1 not in first or node2 not in first:
+        return None
+    buildSparseTable(len(depths))
+    return LCA(node1, node2)
+
 
 '''
 Test Code Here
@@ -477,6 +561,86 @@ def test12():
         print('Test12 FAILED.')
 
 
+#最近公共祖先的RMQ在线算法测试用例
+#            8
+#        6      10
+#       5 7    9  11
+def test13():
+    A1 = BinaryTreeNode(8)
+    A2 = BinaryTreeNode(6)
+    A3 = BinaryTreeNode(10)
+    A4 = BinaryTreeNode(5)
+    A5 = BinaryTreeNode(7)
+    A6 = BinaryTreeNode(9)
+    A7 = BinaryTreeNode(11)
+
+    A1.pLeft, A1.pRight = A2, A3
+    A2.pLeft, A2.pRight = A4, A5
+    A3.pLeft, A3.pRight = A6, A7
+
+    node = lowestCommonAncestor_RMQ(A1, A2, A7)
+    if node == A1:
+        print('Test13 passed.')
+    else:
+        print('Test13 FAILED.')
+
+
+#           1
+#          /
+#         2
+#        /
+#       3
+#      /
+#     4
+#    /
+#   5
+def test14():
+    A1 = BinaryTreeNode(1)
+    A2 = BinaryTreeNode(2)
+    A3 = BinaryTreeNode(3)
+    A4 = BinaryTreeNode(4)
+    A5 = BinaryTreeNode(5)
+
+    A1.pLeft = A2
+    A2.pLeft = A3
+    A3.pLeft = A4
+    A4.pLeft = A5
+
+    node = lowestCommonAncestor_RMQ(A1, A4, A5)
+    if node == A4:
+        print('Test14 passed.')
+    else:
+        print('Test14 FAILED.')
+
+# 当要查询的节点不在树中
+#           1
+#          /
+#         2
+#        /
+#       3
+#      /
+#     4
+#    /
+#   5
+def test15():
+    A1 = BinaryTreeNode(1)
+    A2 = BinaryTreeNode(2)
+    A3 = BinaryTreeNode(3)
+    A4 = BinaryTreeNode(4)
+    A5 = BinaryTreeNode(5)
+    A6 = BinaryTreeNode(6)
+
+    A1.pLeft = A2
+    A2.pLeft = A3
+    A3.pLeft = A4
+    A4.pLeft = A5
+
+    node = lowestCommonAncestor_RMQ(A1, A4, A6)
+    if node == None:
+        print('Test15 passed.')
+    else:
+        print('Test15 FAILED.')
+
 
 if __name__ == '__main__':
     print('当树是二叉搜索树时的测试情况：')
@@ -495,3 +659,7 @@ if __name__ == '__main__':
     test10()
     test11()
     test12()
+    print('RMQ在线算法测试情况：')
+    test13()
+    test14()
+    test15()
