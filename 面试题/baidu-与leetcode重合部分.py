49eb075ad188a359a70d7824ca47d10e2f63188d
waithope <yunqiang.gan@gmail.com>
2019-06-21 15:25:09 +0800

    求一个数的平方根

--- 面试题/baidu-与leetcode重合部分.py
+++ 面试题/baidu-与leetcode重合部分.py
@@ -3,7 +3,7 @@
 题目：求表达式"2*(5+5*2)/3+(6/2+8)"
 见Leetcode 224-Basic Calculator, 227-Basic Calculator II, 772-Basic Calculator III
 
-题目：求一个数的平方根 见 leetcode/math/69-sqrt(x).py
+题目：求一个数的平方根 见 leetcode/math/69-sqrt(x).py 二分法或牛顿法
 
 
 '''
