bcd6d0c6714a513bcf9536cd88e248d0d5efbd6a
waithope <yunqiang.gan@gmail.com>
2019-06-21 11:42:09 +0800

    sqrt(x)

--- 面试题/baidu-与leetcode重合部分.py
+++ 面试题/baidu-与leetcode重合部分.py
@@ -1,7 +1,10 @@
 # -*- coding:utf-8 -*-
 '''
-    基本计算器
-===================
+题目：求表达式"2*(5+5*2)/3+(6/2+8)"
 见Leetcode 224-Basic Calculator, 227-Basic Calculator II, 772-Basic Calculator III
+
+题目：求一个数的平方数 见 leetcode/math/69-sqrt(x).py
+
+
 '''
 
