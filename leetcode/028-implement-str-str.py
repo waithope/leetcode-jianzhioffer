06ba01e808f184c385e721003e357019d81b1ee7
waithope <yunqiang.gan@gmail.com>
2019-09-07 10:50:16 +0800

    KMP

--- leetcode/028-implement-str-str.py
+++ leetcode/028-implement-str-str.py
@@ -13,3 +13,32 @@ class Solution:
                 return i
         return -1
 
+        # KMP算法，时间复杂度O(m + n), 空间复杂度O(n)
+        # def getNext(_next, pattern):
+        #     i, j = 0, -1
+        #     _next[0] = -1
+        #     while i < len(pattern):
+        #         if j == -1 or pattern[i] == pattern[j]:
+        #             i += 1
+        #             j += 1
+        #             _next[i] = j
+        #         else:
+        #             j = _next[j]
+
+        # def KMP(string, pattern, _next):
+        #     i, j = 0, 0
+        #     while j < len(pattern) and i < len(string):
+        #         if j == -1 or string[i] == pattern[j]:
+        #             i += 1
+        #             j += 1
+        #         else:
+        #             j = _next[j]
+        #     if j == len(pattern): #如果匹配，j指针指向len(pattern)
+        #         return i - j
+        #     else:
+        #         return -1
+
+        # _next = [0] * (len(needle) + 1)
+        # getNext(_next, needle)
+        # return KMP(haystack, needle, _next)
+
